640b28eed37b8d499fb205b39b6a413aa2a993e6
Aliteya <112986008+Aliteya@users.noreply.github.com>
2022-11-21 21:08:33 +0300

    Add files via upload

---
+++ 6.2.15.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <cstring>
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    char a[200];
+    int lenw = 0, num = 1;
+
+
+    cin.getline(a, 200);
+
+    int minlen = strlen(a);
+
+    for (int i = 0; i < strlen(a); i++) {
+        if (a[i] != ' ' && a[i] != '\0') {
+            lenw++;
+        }
+        else num++;
+
+        if (lenw) {
+            if (lenw < minlen) {
+                minlen = lenw;
+            }
+
+            lenw = 0;
+        }
+
+    }
+    std::cout << num << "\t" << minlen;
+
+    return 0;
+}
