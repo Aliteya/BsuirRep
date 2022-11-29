785995a921234c41a890a10155ad3eabc288c364
Aliteya <112986008+Aliteya@users.noreply.github.com>
2022-11-29 20:46:50 +0300

    add 8 lab cpp

---
+++ 8.2.15.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+void Out_Rez(double e){
+    cout << e << '\n';
+}
+
+int main()
+{
+    int i = 0;
+    double y, a, b, h, e;
+    cin >> a >> b >> h;
+    for (double x = a; x <= b; x += h, i++)
+    {
+        double k = 1, s = 0, r = 1;
+        y = x * atan(x) - log(sqrt(1 + x * x));
+        do
+        {
+            r *= -1 * x * x;
+            s += r * -1 / (2 * k * (2 * k - 1));
+             e = abs(s - y);
+            Out_Rez(e);
+            k++;
+        } while (e >= 0.1);
+    }
+    cout << "\n" << i;
+    return 0;
+}
