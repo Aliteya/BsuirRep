be4895600b5c3ef185cd584441ba487e1b2c2225
alina <alya.lu2019@mail.ru>
2022-12-18 23:23:48 +0300

    update 7 lab oaip

--- ОАиП/7.2.13.cpp
+++ ОАиП/7.2.13.cpp
@@ -16,11 +16,52 @@ struct stud{
 	int inf = 0;
 	double srball = 0;
 };
+
+void filefill();
+void fileread();
+void fsort();
+void filetask();
+void filedel();
+void update();
+
+int main()
+{
+
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int choise;
+	bool opr = 1;
+	while (opr) {
+	cout << "Выберите" << endl;
+	cout << "1. Создать и записать" << endl;
+	cout << "2. Считать" << endl;
+	cout << "3. Сортировать" << endl;
+	cout << "4. Индивидуалка" << endl;
+	cout << "5. Удаление" << endl;
+	cout << "6. Редактирование" << endl;
+	cout << "7. Завершение работы" << endl;
+	cin >> choise; 
+	switch (choise) {
+	
+	case 1:  filefill();  break;
+	case 2:  fileread();  break;
+	case 3:  fsort(); break;
+	case 4:  filetask(); break;
+	case 5:  filedel(); break;
+	case 6:  update(); break;
+	case 7: return 0; break;
+	default: cout << "Выберите функцию из предложенных" << endl;
+	}
+	}
+	return 0;
+}
 void filefill() {
 	char path[70];
-	cout<< "Введите путь" << endl;
+	cout << "Введите путь" << endl;
 	cin >> path;
-	
+
 	stud student;
 	FILE* my_file;
 	fopen_s(&my_file, path, "wb+");
@@ -40,12 +81,12 @@ void filefill() {
 		cin >> student.mat;
 		cout << "Введите оценку по инфе" << endl;
 		cin >> student.inf;
-	    
+
 		student.srball = (student.fiz + student.mat + student.inf) / 3.;
 		fwrite(&student, sizeof(stud), 1, my_file);
 		cout << "Введите 1, чтобы продолжить, 2 - чтобы остановить";
 		cin >> v;
-	} while ( v != '2');
+	} while (v != '2');
 	fclose(my_file);
 }
 
@@ -66,11 +107,11 @@ void fileread() {
 	fread(student, sizeof(stud), n, my_file);
 	for (int i = 0; i < n; i++) {
 		cout << "Студент номер " << i + 1 << endl;
-		cout << "Фамилия: "  << student[i].surname << endl;
-		cout << "Номер группы: "  << student[i].num << endl;
-		cout << "Оценка по физике: "  << student[i].fiz << endl;
-		cout << "Оценка по матеше: "  << student[i].mat << endl;
-		cout << "Оценка по инфе: "  << student[i].inf << endl;
+		cout << "Фамилия: " << student[i].surname << endl;
+		cout << "Номер группы: " << student[i].num << endl;
+		cout << "Оценка по физике: " << student[i].fiz << endl;
+		cout << "Оценка по матеше: " << student[i].mat << endl;
+		cout << "Оценка по инфе: " << student[i].inf << endl;
 	}
 	delete[] student;
 	fclose(my_file);
@@ -95,11 +136,11 @@ void fsort() {
 	cout << "Выберите сортировку 1- по среднему баллу 2 - по фамилиям";
 	cin >> k;
 	switch (k) {
-	case 1: 
+	case 1:
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = i + 1; j < n; j++) {
-				if ( student[i].srball < student[j].srball)
-				swap(student[i], student[j]);
+				if (student[i].srball < student[j].srball)
+					swap(student[i], student[j]);
 			}
 		}
 
@@ -112,9 +153,9 @@ void fsort() {
 			cout << "Оценка по инфе: " << student[i].inf << endl;
 		}
 
-	 break;
+		break;
 
-	case 2: 
+	case 2:
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = i + 1; j < n; j++) {
 				if (strcmp(student[i].surname, student[j].surname) == true)
@@ -131,7 +172,7 @@ void fsort() {
 			cout << "Оценка по инфе: " << student[i].inf << endl;
 		}
 
-	 break;
+		break;
 	}
 	delete[] student;
 	fclose(my_file);
@@ -159,7 +200,7 @@ void filetask() {
 
 	int n = _filelength(_fileno(my_file)) / sizeof(stud);
 	student = new stud[n];
-	double sum = 0, sr; 
+	double sum = 0, sr;
 	fread(student, sizeof(stud), n, my_file);
 	for (int i = 0; i < n; i++) {
 		sum += student[i].srball;
@@ -168,37 +209,78 @@ void filetask() {
 	fprintf(res_file, "Средний балл по списку - %f \n", sr);
 	for (int i = 0; i < n; i++) {
 		if (student[i].srball > sr) {
-		 fprintf(res_file, "Фамилия - %s \n Номер группы - %d \n Оценка по физике - %d \n Оценка по мaтеше - %d \n Оценка по инфе - %d \n", student[i].surname, student[i].num, student[i].fiz, student[i].mat, student[i].inf);
+			fprintf(res_file, "Фамилия - %s \n Номер группы - %d \n Оценка по физике - %d \n Оценка по мaтеше - %d \n Оценка по инфе - %d \n", student[i].surname, student[i].num, student[i].fiz, student[i].mat, student[i].inf);
 		}
 	}
 	delete[] student;
-	
+
 	fclose(my_file);
 	fclose(res_file);
 }
 
-int main()
-{
-
-	setlocale(LC_ALL, "ru");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
-	int choise, opr = 1;
-	while (opr) {
-	cout << "Выберите" << endl;
-	cout << "1. Создать и записать" << endl;
-	cout << "2. Считать" << endl;
-	cout << "3. Сортировать" << endl;
-	cout << "4. Индивидуалка" << endl;
-
-	cin >> choise; 
-	switch (choise) {
-	case 1:  filefill();  break;
-	case 2:  fileread();  break;
-	case 3:  fsort(); break;
-	case 4: filetask(); break;
+void filedel() {
+	char path[70];
+	cout << "Введите путь" << endl;
+	cin >> path;
+	FILE* my_file;
+	fopen_s(&my_file, path, "rb+");
+	if (!my_file) {
+		cout << "еррор" << endl;
+		return;
 	}
+	int f;
+	cout << "Кого щелкнул Танос?" << endl;
+	cin >> f;
+	stud* student = new stud;
+	fseek(my_file, f * sizeof(stud), SEEK_SET);
+	while (fread(student, sizeof(stud), 1, my_file)) {
+		fseek(my_file, (f - 1) * sizeof(stud), SEEK_SET);
+		fwrite(student, sizeof(stud), 1, my_file);
+		fseek(my_file, ++f * sizeof(stud), SEEK_SET);
 	}
-	return 0;
+	f = _chsize(_fileno(my_file), (f - 1) * sizeof(stud));
+	delete student;
+	fclose(my_file);
+	cout << endl << "Студент отчислен" << endl;
 }
+
+void update() {
+	char path[70];
+	cout << "Введите путь" << endl;
+	cin >> path;
+	FILE* my_file;
+	stud student;
+	fopen_s(&my_file, path, "rb+");
+	if (!my_file) {
+		cout << "еррор" << endl;
+		return;
+	}
+	int number, choise;
+	cout << "Изменить инфу о каком студенте?" << endl;
+	do {
+		cin >> number;
+	} while (number < 0 || number >  _filelength(_fileno(my_file)) / sizeof(stud));
+	cout << "Какую?(1 фамилия, 2 группа, 3 оценка по физике, 4 оценка по матеше, 5 оценка по инфе)" << endl;
+	cin >> choise;
+
+	fseek(my_file, (number - 1) * sizeof(stud), SEEK_SET);
+	fread(&student, sizeof(stud), 1, my_file);
+	switch (choise) {
+	default: cout << "Выберите одну из предложенный операций" << endl;
+	case 1: cin >> student.surname;
+		break;
+	case 2: cin >> student.num;
+		break;
+	case 3: cin >> student.fiz;
+		break;
+	case 4: cin >> student.mat;
+		break;
+	case 5: cin >> student.inf;
+		break;
+	}
+	student.srball = (student.fiz + student.mat + student.inf) / 3.;
+	fseek(my_file, (number - 1) * sizeof(stud), SEEK_SET);
+	fwrite(&student, sizeof(stud), 1, my_file);
+	fclose(my_file);
+}
+
