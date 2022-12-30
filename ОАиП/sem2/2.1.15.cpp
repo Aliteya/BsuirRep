0c80d33bde387019c7ea08e6033ae11ccf385428
alina <alya.lu2019@mail.ru>
2022-12-30 15:07:44 +0300

    divided into sems

---
+++ ОАиП/sem2/2.1.15.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+double rec(int& n) {
+    double y = 0;
+    if (n == 0)
+        return y;
+    else {
+        n--;
+        y = 1. / (1 + rec(n));
+
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << rec(n);
+}
