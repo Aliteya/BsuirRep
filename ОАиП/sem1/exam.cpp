151d17c4140d2d2c9084fa5dad2bd18388c371da
alina <alya.lu2019@mail.ru>
2023-01-11 20:05:05 +0300

    add exam problem

---
+++ ОАиП/sem1/exam.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+void fun(int*, int, double&, bool&);
+
+int main()
+{
+    int N;
+    bool s = true;
+    double k = 0;
+    cin >> N;
+    int* A = new int[N];
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+
+    for (int i = 0; i < N; i++)
+        cout << A[i]<< " ";
+    cout << endl;
+    fun(A, N, k, s);
+    for (int i = 0; i < N; i++) 
+        cout << A[i] << " ";
+    cout << endl;
+    if (s == false)
+        cout << k;
+
+    return 0;
+}
+void fun(int* A, int N, double& k, bool& s) {
+    int y = 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] > 0) {
+            k += A[i];
+            y++;
+            s = false;
+          
+        }
+        else if (A[i] < 0)
+            A[i] *= 2;
+    }
+    k /= y;
+}
