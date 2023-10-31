5b399e98d4f2b909840bfbff18d2007aec7ed47e
alina <alya.lu2019@mail.ru>
2023-10-31 16:34:32 +0300

    fabriicsys2.0

--- 2 курс/ППОИС/LAB3/fabriic_system.cpp
+++ 2 курс/ППОИС/LAB3/fabriic_system.cpp
@@ -4,42 +4,44 @@
 #include <map>
 #include <algorithm>
 
-class Human{
+class Factory{
 public:
     std:: string name;
+    int profit;
+
+    Factory(){
+        name = "Workbench";
+        profit = 1000;
+    }
+
+    Factory(std::string _name, int _profit){
+        name = _name;
+        profit = _profit;
+    }
+
+    void add_profit(int _price){
+        profit += _price;
+    }
+
+    void subtract_profit(int _cost){
+        profit -= _cost;
+    }
 };
 
-class Employee: public Human{
-protected:
+class Product{
+public:
     int id;
-    int salary;
-public:
-    virtual void work()=0;
-};
+    int price;
+    std::string product_name;
 
-class Manager: public Employee{
-protected:
-    void add_subordinates(){}
-    void remove_subordinates(){}
-public:
-    std::vector <Employee> subordinates;
-    void work() override{}
-};
+    Product(){}
 
-class Engineer: public Employee{
-public:
-    void work() override{}
-};
-
-class Machine{
-public:
-    std ::string name;
-    int id;
-    std::string type;
-    int status;
-
-    void start(){}
-    void finish(){}
+    Product(int _id, int _price, std::string _prod_name){
+        id = _id;
+        price = _price;
+        product_name = _prod_name;
+    };
+   ~Product(){}
 };
 
 class RawMaterial{
@@ -58,26 +60,12 @@ public:
             amount = _amount;
             std::cout << "Raw material: " << name << " " << amount << std::endl;
         } 
-        else throw "We don't provide this material: " + _name;
+        else throw std:: runtime_error("We don't provide this material: " + _name);
     }
 
+    ~RawMaterial(){}
 };
 
-class Product{
-public:
-    int id;
-    int price;
-    std::string product_name;
-
-    Product() {}
-
-    Product(int _id, int _price, std::string _prod_name){
-        id = _id;
-        price = _price;
-        product_name = _prod_name;
-    };
-   // void manufacture();
-};
 
 class ProductionLine{
 public:
@@ -99,7 +87,7 @@ public:
         product_map[{"metal", "glass"}] = Product(6, 100, "glasses");
     }
 
-    Product manufacture(RawMaterial& mater1, RawMaterial& mater2){
+    Product manufacture(RawMaterial& mater1, RawMaterial& mater2, Factory& fact){
         auto it = product_map.find({mater1.name, mater2.name});
         if (it != product_map.end()){
             Product product = it->second;
@@ -107,47 +95,210 @@ public:
             return product;
         }
         else {
-            throw "No product found for the given materials";
+            throw std::runtime_error("No product found for the given materials");
         }
     }
 };
 
-
-class Supplier: public Human{
+class Machine{
+protected:
+    int status;
 public:
-    Supplier(){
+    std ::string name;
+    int id;
+    std::string type;
+
+    Machine(){
+        name = "Stanok";
+        id = 221703;
+        type = "Magic mixer";
+        status = 0;
     }
 
+    Machine(std::string _name, int _id, std::string _type){
+        name = _name;
+        id = _id;
+        type = _type;
+        status = 0;
+    }
 
-    void provide_materials(RawMaterial& mater){
-        if (std::find(mater.materials.begin(), mater.materials.end(), mater.name) != mater.materials.end()) {
-            std::cout << "Providing material: " << mater.name << std::endl;
-            mater.amount += 5;
-        } 
-        else std::cout << "Material not allowed: " << mater.name << std::endl;
+    ~Machine(){}
+
+    void start(){
+        status = 1;
+        std::cout << "Machine started working" << std::endl;
+    }
+
+    void finish(){
+        status = 0;
+        std::cout << "Machine finished working" << std::endl;
+    }
+
+    Product produce(RawMaterial& mater1, RawMaterial& mater2, ProductionLine& line, Factory& fact){
+        start();
+        Product product = line.manufacture(mater1, mater2, fact);
+        finish();
+        return product;
     }
 };
 
-class Customer: public Human{};
+class Human {
+public:
+    std::string name;
 
-class Factory{};
+    Human() {
+        name = "Ryan Gosling";
+    }
+
+    Human(std::string _name) {
+        name = _name;
+    }
+
+    ~Human() {}
+};
+
+class Employee : public Human {
+protected:
+    int id;
+    int salary;
+    Factory* factory;
+
+public:
+    Employee() {
+        id = 0;
+        salary = 0;
+        factory = nullptr;
+    }
+
+    Employee(std::string _name, int _id, int _salary, Factory* _factory) {
+        name = _name;
+        id = _id;
+        salary = _salary;
+        factory = _factory;
+    }
+
+    ~Employee() {}
+
+    virtual void work() = 0;
+
+    int get_salary() {
+        return salary;
+    }
+
+    int get_id() {
+        return id;
+    }
+};
+
+class Engineer : public Employee {
+public:
+    Engineer() {}
+
+    Engineer(std::string _name, int _id, int _salary, Factory* _factory) {
+        name = _name;
+        id = _id;
+        salary = _salary;
+        factory = _factory;
+    }
+
+    ~Engineer() {}
+
+    void work() override {
+        std::cout << "Engineer is working" << std::endl;
+    }
+
+    Product operateMachine(Machine& machine, RawMaterial& mater1, RawMaterial& mater2, ProductionLine& line, Factory& fact) {
+        std::cout << "Engineer is operating the machine" << std::endl;
+        return machine.produce(mater1, mater2, line, fact);
+    }
+};
+
+class Manager : public Employee {
+public:
+    std::vector<Engineer*> subordinates;
+
+    Manager() {}
+
+    Manager(std::string _name, int _id, int _salary, Factory* _factory) {
+        name = _name;
+        id = _id;
+        salary = _salary;
+        factory = _factory;
+    }
+
+    ~Manager() {}
+
+    void work() override {
+        std::cout << "Manager is working" << std::endl;
+    }
+
+    void add_subordinates(Engineer* _engineer) {
+        subordinates.push_back(_engineer);
+    }
+
+    void pay_salary() {
+        for (int i = 0; i < subordinates.size(); i++) {
+            double salary = subordinates[i]->get_salary();
+            factory->subtract_profit(salary);
+            std::cout << "Paying $" << salary << " to " << subordinates[i]->get_id() << std::endl;
+        }
+    }
+};
+
+class Supplier : public Human {
+public:
+    std::map<std::string, int> material_prices = {{"wood", 100}, {"wool", 200}, {"metal", 300}, {"web", 20}, {"glass", 50}};
+
+    Supplier() {}
+
+    ~Supplier() {}
+
+    void provide_materials(RawMaterial& mater, Factory& fact) {
+        if (std::find(mater.materials.begin(), mater.materials.end(), mater.name) != mater.materials.end()) {
+            std::cout << "Providing material: " << mater.name << std::endl;
+            mater.amount += 5;
+            int cost = material_prices[mater.name];
+            fact.subtract_profit(cost);
+        } else {
+            std::cout << "Material not allowed: " << mater.name << std::endl;
+        }
+    }
+};
+
+class Customer : public Human {
+public:
+    std::vector<std::pair<std::string, int>> receipt;
+
+    void buy_product(Product& product, Factory& factory) {
+        receipt.push_back({product.product_name, product.price});
+        factory.add_profit(product.price);
+        std::cout << "Bought product: " << product.product_name << std::endl;
+    }
+};
 
 int main(){
 
   try {
-        // Create RawMaterials
-        RawMaterial wood("wood", 10);
-        RawMaterial wool("wool", 5);
-        RawMaterial metal("metal", 3);
-        Supplier supplier1;
+    Factory factory;
+    RawMaterial rawMaterial("wood", 5);
+    RawMaterial rawMaterial2("wool", 6);
+    ProductionLine productionLine;
+    Machine machine("Stanok", 221703, "Magic mixer");
+    Engineer engineer("John Doe", 123, 5000, &factory);
+    Manager manager("Jane Smith", 456, 7000, &factory);
+    Supplier supplier;
+    Customer customer;
 
-        // Provide materials to the Supplier
-        supplier1.provide_materials(wood);
-        supplier1.provide_materials(wool);
-        supplier1.provide_materials(metal);
-
-        ProductionLine prline;
-        prline.manufacture(wood, wool);
+    factory.add_profit(100);
+    factory.subtract_profit(50);
+    Product product = productionLine.manufacture(rawMaterial, rawMaterial2, factory);;
+    engineer.work();
+    Product producedProduct = engineer.operateMachine(machine, rawMaterial, rawMaterial2, productionLine, factory);
+    manager.work();
+    manager.add_subordinates(&engineer);
+    manager.pay_salary();
+    supplier.provide_materials(rawMaterial, factory);
+    customer.buy_product(product, factory);
     } catch (const std::string& error_message) {
         std::cout << "Error: " << error_message << std::endl;
     }
