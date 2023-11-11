9f50dc0588edfcec4ebb84715bda5c6fa7cd785b
Alitey <alyalu004@gmail.com>
2023-11-11 13:26:56 +0300

    clean

--- 2 курс/ППОИС/fabriic_system2/build/googletest-1.14.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/fabriic_system2/googletest-1.14.0/googlemock
-# Build directory: D:/fabriic_system2/build/googletest-1.14.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- 2 курс/ППОИС/fabriic_system2/build/googletest-1.14.0/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/fabriic_system2/googletest-1.14.0/googletest
-# Build directory: D:/fabriic_system2/build/googletest-1.14.0/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
