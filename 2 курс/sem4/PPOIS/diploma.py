e024de92b8e8fef5f8a00ed7110b1a813b1b9000
Alitey <alyalu004@gmail.com>
2024-02-18 16:03:57 +0300

    fix

--- 2 курс/sem4/PPOIS/diploma.py
+++ 2 курс/sem4/PPOIS/diploma.py
@@ -3,11 +3,20 @@ class Human:
         self.name = name
         self.age = age
 
+from random import choice 
+
 class Teacher(Human):
     def __init__(self, name, age, experience=5, degree="assistant"): 
         super().__init__(name, age)
         self.experience = experience
         self.degree = degree
+
+    def asking() -> str:
+        vocab: list = [ "what is my name?", "what do you know about temporal relations?",
+                        "why aren't you a fan yet?", "The universe is infinite?",
+                        "when will I become happy?", "Are my eyes glued to you again?",
+                        "My Love Mine All Mine?", "where is my mind?", "and listened to the Pacific Ocean and saw cities?"]
+        return choice(vocab)
 
 class Diploma():
     def __init__(self, title="Hello world", pages=30, count_sources=1):
@@ -28,14 +37,12 @@ class Student(Human):
         super().__init__(name, age)
         self.diploma = diploma
     
-    @classmethod
     def preparing_presentation_report(self) -> Presentation:
-        pres_title = f"genuis title for presentation {self.name}'s "
-        author = self.name
-        content = f"some slides about {self.diploma.title}... Diploma is {self.diploma.pages} pages long and has {self.diploma.count_sources} sources"
+        pres_title: str = f"genuis title for presentation {self.name}'s "
+        author: str = self.name
+        content: str = f"some slides about {self.diploma.title}... Diploma is {self.diploma.pages} pages long and has {self.diploma.count_sources} sources"
         return Presentation(pres_title, author, content)
 
-    @classmethod
     def reviewing_correcting_report(self) -> str:
         if self.diploma.checked == True:
             return "Okey, my diploma is checked"
@@ -44,12 +51,11 @@ class Student(Human):
             self.count_sources += 3
             return "Yes, I try to correct"
            
-    @classmethod
     def conducting_defense_rehearsals():
         pass
     
-    @classmethod
-    def answering():
+    def answering(que1: str, que2: str, que3: str):
+
         pass
     
 class Supervisor(Teacher):  
@@ -68,7 +74,12 @@ class Comission():
         self.teacher2 = teacher2
         self.teacher3 = teacher3
 
-    @classmethod
+    def commission_asking(self) -> tuple:
+        que1: str = self.teacher1.asking()
+        que2: str = self.teacher2.asking()
+        que3: str = self.teacher3.asking()
+        return que1, que2, que3
+
     def certification_grade():
         pass
     pass
@@ -76,4 +87,7 @@ class Comission():
 
 
 def main():
-    pass
+    dip1 = Diploma()
+    bob = Student("bob", 24, dip1) 
+    print(bob.preparing_presentation_report())
+main()
